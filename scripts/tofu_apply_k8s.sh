ce3d61c4ae36f7e9e3996e4b52531c625b90b6b1
Dudi Zimberknopf <dudi@vizo.dev>
2024-01-25 15:42:12 +0200

    fix args position

--- scripts/tofu_apply_k8s.sh
+++ scripts/tofu_apply_k8s.sh
@@ -2,4 +2,4 @@ PLAN_PATH=local-k8s
 if [ -n "$1" ]; then
   PLAN_PATH=$1
 fi
-cd ../tofu && tofu apply $PLAN_PATH -state-out=../state/state-k8s
+cd ../tofu && tofu apply -state-out=../state/state-k8s $PLAN_PATH
