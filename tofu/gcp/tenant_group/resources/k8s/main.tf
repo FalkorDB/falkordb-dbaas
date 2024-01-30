1c8fb9cb08587dda52fed2687a5ee5da6257e88e
Dudi Zimberknopf <dudi@vizo.dev>
2024-01-30 16:43:57 +0200

    add gke sa to gcp bind

--- tofu/gcp/tenant_group/resources/k8s/main.tf
+++ tofu/gcp/tenant_group/resources/k8s/main.tf
@@ -1,30 +1,58 @@
-
+locals {
+  service_account_name = split("@", var.tenant_provision_sa)[0]
+}
 resource "kubernetes_role" "tenant_provision_sa_role" {
-    metadata {
-        name = "provisioner-sa-role"
+  metadata {
+    name = "provisioner-sa-role"
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+resource "kubernetes_service_account" "tenant_provision_sa" {
+  metadata {
+    name = local.service_account_name
+    annotations = {
+      "iam.gke.io/gcp-service-account" = var.tenant_provision_sa
     }
-    
-    rule {
-        api_groups = ["*"]
-        resources  = ["*"]
-        verbs      = ["*"]
-    }
+  }
 }
 
 resource "kubernetes_role_binding" "tenant_provision_sa_role_binding" {
-    metadata {
-        name = "provisioner-sa-role-binding"
-    }
-    
-    role_ref {
-        api_group = "rbac.authorization.k8s.io"
-        kind      = "Role"
-        name      = kubernetes_role.tenant_provision_sa_role.metadata[0].name
-    }
-    
-    subject {
-        kind      = "ServiceAccount"
-        name      = var.tenant_provision_sa
-        namespace = "default"
-    }
+  metadata {
+    name = "provisioner-sa-role-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.tenant_provision_sa_role.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.tenant_provision_sa.metadata[0].name
+    namespace = "default"
+  }
 }
+
+# Bind the GCP Service Account to the Kubernetes Service Account
+data "google_iam_policy" "tenant_provision_sa_binding_policy" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+    members = [
+      "serviceAccount:${var.project_id}.svc.id.goog[default/${local.service_account_name}]"
+    ]
+  }
+}
+
+resource "google_service_account_iam_policy" "tenant_provision_sa_binding" {
+  service_account_id = var.tenant_provision_sa
+  policy_data        = data.google_iam_policy.tenant_provision_sa_binding_policy.policy_data
+}
+
+
