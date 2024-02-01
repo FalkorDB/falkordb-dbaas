0f0a421c426972879fe803a91b7caf03bf940cd6
Dudi Zimberknopf <dudi@vizo.dev>
2024-02-01 14:56:18 +0200

    add cluster role permissions to provisioning sa

--- tofu/gcp/tenant_group/resources/k8s/main.tf
+++ tofu/gcp/tenant_group/resources/k8s/main.tf
@@ -1,45 +1,44 @@
-# locals {
-#   service_account_email = reverse(split("/", var.tenant_provision_sa))[0]
-#   service_account_name  = split("@", local.service_account_email)[0]
-# }
-# resource "kubernetes_role" "tenant_provision_sa_role" {
-#   metadata {
-#     name = "provisioner-sa-role"
-#   }
+locals {
+  service_account_email = reverse(split("/", var.tenant_provision_sa))[0]
+  service_account_name  = split("@", local.service_account_email)[0]
+}
+resource "kubernetes_cluster_role" "tenant_provision_sa_role" {
+  metadata {
+    name = "provisioner-sa-role"
+  }
 
-#   rule {
-#     api_groups = ["*"]
-#     resources  = ["*"]
-#     verbs      = ["*"]
-#   }
-# }
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
 
-# resource "kubernetes_service_account" "tenant_provision_sa" {
-#   metadata {
-#     name = local.service_account_name
-#     annotations = {
-#       "iam.gke.io/gcp-service-account" = local.service_account_email
-#     }
-#   }
-# }
+resource "kubernetes_service_account" "tenant_provision_sa" {
+  metadata {
+    name = local.service_account_name
+    annotations = {
+      "iam.gke.io/gcp-service-account" = local.service_account_email
+    }
+  }
+}
 
-# resource "kubernetes_role_binding" "tenant_provision_sa_role_binding" {
-#   metadata {
-#     name = "provisioner-sa-role-binding"
-#   }
+resource "kubernetes_cluster_role_binding" "tenant_provision_sa_role_binding" {
+  metadata {
+    name = "provisioner-sa-role-binding"
+  }
 
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "Role"
-#     name      = kubernetes_role.tenant_provision_sa_role.metadata[0].name
-#   }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.tenant_provision_sa_role.metadata[0].name
+  }
 
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = kubernetes_service_account.tenant_provision_sa.metadata[0].name
-#     namespace = "default"
-#   }
-# }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.tenant_provision_sa.metadata[0].name
+  }
+}
 
 # # Bind the GCP Service Account to the Kubernetes Service Account
 # Try to move this to control plane project to prevent tenant group from deleting it
