910973779f70419250bbe7113415ca21178b566c
Dudi Zimberknopf <dudi@vizo.dev>
2024-02-01 15:07:09 +0200

    fix var

--- tofu/gcp/tenant_group/resources/dns/main.tf
+++ tofu/gcp/tenant_group/resources/dns/main.tf
@@ -1,7 +1,7 @@
 
 resource "google_dns_managed_zone" "dns_zone" {
   project = var.project_id
-  name    = "${tenant_group_name}-dns"
+  name    = "${var.tenant_group_name}-dns"
 
   dns_name   = "${var.tenant_group_name}.${var.dns_domain}."
   visibility = "public"
