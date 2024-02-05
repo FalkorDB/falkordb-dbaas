22aaeed50cbe04472b3c46699c41bd1fd1ab770e
Dudi Zimberknopf <dudi@vizo.dev>
2024-02-05 18:24:56 +0200

    force destroy zone

--- tofu/gcp/tenant_group/resources/dns/main.tf
+++ tofu/gcp/tenant_group/resources/dns/main.tf
@@ -3,8 +3,9 @@ resource "google_dns_managed_zone" "dns_zone" {
   project = var.project_id
   name    = "${var.tenant_group_name}-dns"
 
-  dns_name   = "${var.tenant_group_name}.${var.dns_domain}."
-  visibility = "public"
+  dns_name      = "${var.tenant_group_name}.${var.dns_domain}."
+  visibility    = "public"
+  force_destroy = true
 }
 
 # Create DNS Service account
