f5c3485018de5254b9398265f81f30313d9f60e5
Dudi Zimberknopf <dudi@vizo.dev>
2024-02-08 13:58:11 +0200

    wait for metrics server

--- tofu/gcp/tenant_group/resources/k8s/monitoring/main.tf
+++ tofu/gcp/tenant_group/resources/k8s/monitoring/main.tf
@@ -95,13 +95,31 @@
 #   }
 # }
 
-resource "null_resource" "falkordb_pod_monitoring" {
+resource "null_resource" "k8s_credentials" {
   provisioner "local-exec" {
     when    = create
     command = <<EOF
     gcloud container clusters get-credentials ${var.cluster_name} --region ${var.region} --project ${var.project_id}
+    # Wait for the GMP operator to be available
     kubectl wait --for=condition=Available --timeout=5m -n gmp-system deployment/gmp-operator
-    kubectl apply -f - <<EOF2
+    # Wait for the metrics server to be available
+    kubectl wait --for=condition=Available --timeout=5m deployment -n kube-system -l k8s-app=metrics-server
+    EOF
+  }
+
+}
+
+resource "time_sleep" "sleep" {
+  depends_on      = [null_resource.k8s_credentials]
+  create_duration = "10s"
+}
+
+
+resource "null_resource" "pod_monitoring" {
+  provisioner "local-exec" {
+    when    = create
+    command = <<EOF
+      kubectl apply -f - <<EOF2
 apiVersion: monitoring.googleapis.com/v1
 kind: ClusterPodMonitoring
 metadata:
@@ -117,8 +135,7 @@ spec:
   - port: metrics
     interval: 30s
 EOF2
-EOF
-
+    EOF
   }
-
+  depends_on = [time_sleep.sleep]
 }
