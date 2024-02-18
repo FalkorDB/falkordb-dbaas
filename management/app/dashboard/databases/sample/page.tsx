35f61ab9af21d5c0d33a8e291d6927ac8da40487
Guy Korland <gkorland@gmail.com>
2024-02-18 16:45:19 +0200

    add sample info

--- management/app/dashboard/databases/sample/page.tsx
+++ management/app/dashboard/databases/sample/page.tsx
@@ -3,18 +3,37 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Info, Monitor } from "lucide-react";
 import { useState } from "react";
 
-const SAMMPLES = [
-    "Sample1",
-    "Sample2",
-    "Sample3",
+interface Sample {
+    name: string;
+    description: string;
+    machine: string;
+}
+
+const SAMPLES: Sample[] = [
+    {
+        name: "Sample 1",
+        description: "This is a sample data description 1",
+        machine: "m1.small"
+    },
+    {
+        name: "Sample 2",
+        description: "This is a sample data description 2",
+        machine: "m1.small"
+    },
+    {
+        name: "Sample 3",
+        description: "This is a sample data description 3",
+        machine: "m1.small"
+    }
 ]
 
 
 export default function Page() {
 
-    const [sample, setSample] = useState(SAMMPLES[0]);
+    const [sample, setSample] = useState(SAMPLES[0].name);
 
     const createDatabase = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -26,7 +45,7 @@ export default function Page() {
             })
         }).then((res) => {
             if (res.ok) {
-                console.log('Database created')
+                console.log('Sample loaded')
             }
         })
     }
@@ -38,19 +57,32 @@ export default function Page() {
                 <form className="flex flex-col space-y-10" onSubmit={createDatabase}>
                     <div className="flex flex-row space-x-4 items-center">
                         <Label className="text-4xl min-w-fit p-2 text-left" htmlFor="machineType">Sample Data:</Label>
-                        <Select onValueChange={setSample} defaultValue={SAMMPLES[0]}>
+                        <Select onValueChange={setSample} defaultValue={SAMPLES[0].name}>
                             <SelectTrigger id="machineType" className="text-4xl p-8 border-8" >
                                 <SelectValue placeholder="Sample" />
                             </SelectTrigger>
                             <SelectContent>
                                 {
-                                    SAMMPLES.map((item) => (
-                                        <SelectItem className="text-4xl" key={item} value={item}>{item}</SelectItem>
+                                    SAMPLES.map((item) => (
+                                        <SelectItem className="text-4xl" key={item.name} value={item.name}>{item.name}</SelectItem>
                                     ))
                                 }
                             </SelectContent>
                         </Select>
                     </div>
+                    {sample &&
+                        <div>
+                            <div className="p-8 text-4xl flex flex-row items-center space-x-2">
+                                <Info />
+                                <div>{SAMPLES.find((s) => s.name === sample)?.description}</div>
+                            </div>
+                            <div className="p-8 text-4xl flex flex-row items-center space-x-2">
+                                <Monitor />
+                                <div>{SAMPLES.find((s) => s.name === sample)?.machine}</div>
+                            </div>
+                        </div>
+
+                    }
                     <Button className="p-8 text-4xl">Load Sample</Button>
                 </form>
             </main>
