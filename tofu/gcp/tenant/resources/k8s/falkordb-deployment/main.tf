4a8b53314195f617d362831a6530f437bf26d670
Dudi Zimberknopf <dudi@vizo.dev>
2024-03-04 16:44:51 +0200

    fix commonConfiguration

--- tofu/gcp/tenant/resources/k8s/falkordb-deployment/main.tf
+++ tofu/gcp/tenant/resources/k8s/falkordb-deployment/main.tf
@@ -76,4 +76,6 @@ module "labeler" {
   deployment_name      = local.deployment_name
   sentinel_port        = var.sentinel_port
   labeler_image        = var.labeler_image
+
+  depends_on = [ module.multi_zone, module.single_zone, module.standalone ]
 }
