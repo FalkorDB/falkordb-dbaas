98c4cbec580f5a8f66cb47d6dd4ddb0c41835ffa
Dudi Zimberknopf <zimber.dudi@gmail.com>
2025-04-03 11:07:05 +0300

    check for sa authorization

--- frontend/apps/auth-proxy/app/api/verify/route.ts
+++ frontend/apps/auth-proxy/app/api/verify/route.ts
@@ -5,6 +5,17 @@ import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
   const session = await getServerSession(authOptions);
 
+  // get basic auth
+  const basicAuth = req.headers.get("Authorization");
+  if (basicAuth) {
+    const [username, password] = Buffer.from(basicAuth.split(" ")[1] ?? "", "base64")
+      .toString()
+      .split(":");
+    if (username !== process.env.GRAFANA_SA_USERNAME || password !== process.env.GRAFANA_SA_PASSWORD) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+  }
+
   if (!session || !(session.user?.email || (session.user as any)?.id || (session.user as any)?.name)) {
     // check if grafana_session cookie is set
     const grafanaSession = req.cookies.get("grafana_session");
