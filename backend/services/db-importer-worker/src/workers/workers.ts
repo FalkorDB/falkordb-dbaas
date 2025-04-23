ca04d99a9e5bc4555db21daa4d4ef1d7ff0e5c50
Dudi Zimberknopf <zimber.dudi@gmail.com>
2025-04-23 12:50:41 +0300

    add REDIS_URL fix psuh pipeline

--- backend/services/db-importer-worker/src/workers/workers.ts
+++ backend/services/db-importer-worker/src/workers/workers.ts
@@ -12,10 +12,7 @@ export const setupWorkers = () => {
   for (const { name, processor, concurrency } of processors) {
     new Worker(name, processor, {
       connection: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: Number(process.env.REDIS_PORT) || 6379,
-        username: process.env.REDIS_USERNAME || 'default',
-        password: process.env.REDIS_PASSWORD || 'default',
+        url: process.env.REDIS_URL,
       },
       concurrency: concurrency ?? process.env.WORKER_CONCURRENCY ? Number(process.env.WORKER_CONCURRENCY) : 1,
     });
--- backend/services/db-importer/src/repositories/tasksQueue/TaskQueueBullMQRepository.ts
+++ backend/services/db-importer/src/repositories/tasksQueue/TaskQueueBullMQRepository.ts
@@ -17,10 +17,7 @@ export class TaskQueueBullMQRepository {
   ) {
     this._producer = new FlowProducer({
       connection: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: Number(process.env.REDIS_PORT) || 6379,
-        username: process.env.REDIS_USERNAME || 'default',
-        password: process.env.REDIS_PASSWORD || 'default',
+        url: process.env.REDIS_URL,
       },
     });
   }
